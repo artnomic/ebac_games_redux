0293b53c2eb54e8da2dbc1c3bacdcb4077f64676
artnomic <arthurpe44@gmail.com>
2024-12-02 19:09:43 -0300

    feat: create dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react'
+import { Provider } from 'react-redux'
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
 
 import { GlobalStyle } from './styles'
+
+import { store } from './store'
 
 export type Game = {
   id: number
@@ -16,7 +19,6 @@ export type Game = {
 
 function App() {
   const [games, setGames] = useState<Game[]>([])
-  const [carrinho, setCarrinho] = useState<Game[]>([])
 
   useEffect(() => {
     fetch('http://localhost:8080/produtos')
@@ -25,13 +27,13 @@ function App() {
   }, [])
 
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <div className="container">
-        <Header itensNoCarrinho={carrinho} />
-        <Produtos jogos={games} adicionarAoCarrinho={adicionarAoCarrinho} />
+        <Header />
+        <Produtos jogos={games} />
       </div>
-    </>
+    </Provider>
   )
 }
 
