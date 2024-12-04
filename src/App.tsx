7721ea912d4127f3c19266e0946ba58cfc21f63c
artnomic <arthurpe44@gmail.com>
2024-12-04 15:24:17 -0300

    feat: api routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
@@ -18,20 +17,12 @@ export type Game = {
 }
 
 function App() {
-  const [games, setGames] = useState<Game[]>([])
-
-  useEffect(() => {
-    fetch('http://localhost:8080/produtos')
-      .then((res) => res.json())
-      .then((res) => setGames(res))
-  }, [])
-
   return (
     <Provider store={store}>
       <GlobalStyle />
       <div className="container">
         <Header />
-        <Produtos jogos={games} />
+        <Produtos />
       </div>
     </Provider>
   )
